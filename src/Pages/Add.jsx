602d058a013f07fab16eaac011576ea57319c4da
Leksa <admin@petiteweb.ru>
2022-11-02 13:56:30 +0300

    Add api

--- src/Pages/Add.jsx
+++ src/Pages/Add.jsx
@@ -2,10 +2,20 @@ import React, {useEffect, useState} from "react";
 import Api from "../api.js";
 
 export default () => {
+    const [type, setType] = useState("");
+    const handler = (e) => {
+        e.preventDefault();
+        const body =  {
+            "type": type
+        }
+        console.log(body);
+        // const result = await Api.add(body);
+        // alert(JSON.stringify(result));
+    }
     return <main>
         <h1>Добавить информацию</h1>
-        <form>
-            <input type="text" name="type" placeholder="Название вида"/>
+        <form onSubmit={handler}>
+            <input type="text" name="type" placeholder="Название вида" value={type} onChange={e => setType(e.target.value)}/>
             <button type="submit">Добавить в БД</button>
         </form>
     </main>
--- src/api.js
+++ src/api.js
@@ -1,13 +1,12 @@
 class Api {
-    static PATH = "https://wwf-server.herokuapp.com/api/animals";
-    static getAll() {
-        return fetch(this.PATH).then(res => res.json()).then(data => data);
+    static async getAll(path = "https://wwf-server.herokuapp.com/api/animals") {
+        return fetch(path).then(res => res.json()).then(data => data);
     }
-    static getSingle(id) { // _id
-        return fetch(this.PATH + "/" + id).then(res => res.json()).then(data => data);
+    static async getSingle(id, path = "https://wwf-server.herokuapp.com/api/animals") { // _id
+        return fetch(path + "/" + id).then(res => res.json()).then(data => data);
     }
-    static add(body) {
-        return fetch(this.PATH + "/add", {
+    static async add(body, path = "https://wwf-server.herokuapp.com/api/animals") {
+        return fetch(path + "/add", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -16,8 +15,8 @@ class Api {
             body: JSON.stringify(body)
         }).then(res => res.json()).then(data => data);
     }
-    static update(id, body) {
-        return fetch(this.PATH + "/update/" + id, {
+    static async update(id, body, path = "https://wwf-server.herokuapp.com/api/animals") {
+        return fetch(path + "/update/" + id, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
@@ -27,3 +26,4 @@ class Api {
         }).then(res => res.json()).then(data => data);
     }
 }
+export default Api;
